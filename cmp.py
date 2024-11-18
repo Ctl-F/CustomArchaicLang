8ace1978c56fc0f19ddf3fbd572ff91e8cfeafed
spencer brough <ctlf@DESKTOP-RAIDHB3>
2024-11-18 04:15:46 -0700

    Small expansion to compilation

--- cmp.py
+++ cmp.py
@@ -6,14 +6,27 @@ header_filename = ""
 source_filename = ""
 header_body = []
 source_body = []
+source_includes = []
+source_functions = []
+global_functions = []
 
 def append_all(list, *args):
     for arg in args:
         list.append(arg)
     return list
 
+def compile_function(function_node):
+    pass
+
+def compile_cinclude(include_node):
+    global source_includes
+    source_includes.append("#include <%s>\n" % include_node[1:-1])
+    pass
+
 def compile_lib(tree):
     global header_filename, header_body, source_filename, source_body
+    global source_includes, source_functions, global_functions
+    
     lib_name = "calib" + tree.children[0]
     
     header_filename = lib_name + ".h"
@@ -21,10 +34,22 @@ def compile_lib(tree):
 
     header_body = append_all(header_body, "#ifndef __", lib_name, "__\n")
     header_body = append_all(header_body, "#define __", lib_name, "__\n\n")
-    source_body = append_all(source_body, "#include \"", lib_name, ".h\"\n\n")
+    source_includes = append_all(source_includes, "#include \"", lib_name, ".h\"\n\n")
 
+    functions = []
+
+    unit_body = tree.children[1]
+
+    for item in unit_body.children:
+        match item.data:
+            case "c_include":
+                compile_cinclude(item.children[0])
+            case "function":
+                compile_function(item)
+            case _:
+                print("Unexpected item %s" % item)
+                return
     
-
 
 
     header_body = append_all(header_body, "\n#endif //__", lib_name, "__\n")
@@ -32,12 +57,16 @@ def compile_lib(tree):
         f.write("".join(header_body))
 
     with open(source_filename, "w") as f:
+        f.write("".join(source_includes))
         f.write("".join(source_body))
 
     header_filename = ""
     source_filename = ""
     header_body = []
     source_body = []
+    source_includes = []
+    source_functions = []
+    global_functions = []
     pass
 
 def compile(tree):
